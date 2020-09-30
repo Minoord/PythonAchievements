e73425bc601589a69a3084ed2c93c495ad9e93ee
Michelle <Minoord18@outlook.com>
2020-09-30 11:59:51 +0200

    Dit is de opdracht MyFirstOperation

---
+++ PYTB1L2MyFirstOperations/PYTB1L2MyFirstOperations.py
@@ -0,0 +1,33 @@
+L = 2 + 2
+print( "2 + 2 =")
+print (L)
+print ("--------------------------------------------")
+X = 3-2
+print ("3 - 2 =") 
+print(X)
+print ("--------------------------------------------")
+H = 40 / 5
+print ("40 / 5 =") 
+print ( H )
+print ("--------------------------------------------")
+
+Number= 45
+print ("Number= 45")
+print ( Number )
+print ("--------------------------------------------")
+Nieuwnumber = Number - 10
+print ("45 - 10 =")
+print ( Nieuwnumber )
+print ("--------------------------------------------")
+Nnumber = Nieuwnumber + 1
+print ("35 + 1 =")
+print ( Nnumber )
+print ("--------------------------------------------")
+Getal = Nnumber / 2
+print ("36 / 2 =")
+print ( Getal)
+print ("--------------------------------------------")
+restWaarde = Getal % 4
+print ("18 % 4 =")
+print ( restWaarde ) 
+
