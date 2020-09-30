9c8b5b39b1f078129385be8ddd9bcb7eac993f47
Michelle <Minoord18@outlook.com>
2020-09-30 12:33:52 +0200

    Ik was vergeten wat sommetjes er bij toe te gevoegen. Ze zijn nu toegevoegd

--- PYTB1L2MyFirstOperations/PYTB1L2MyFirstOperations.py
+++ PYTB1L2MyFirstOperations/PYTB1L2MyFirstOperations.py
@@ -6,28 +6,49 @@ X = 3-2
 print ("3 - 2 =") 
 print(X)
 print ("--------------------------------------------")
+G = 3*92
+print ("3 * 92 =") 
+print(G)
+print ("--------------------------------------------")
 H = 40 / 5
 print ("40 / 5 =") 
 print ( H )
 print ("--------------------------------------------")
 
+# variable =
 Number= 45
 print ("Number= 45")
 print ( Number )
 print ("--------------------------------------------")
-Nieuwnumber = Number - 10
-print ("45 - 10 =")
-print ( Nieuwnumber )
-print ("--------------------------------------------")
-Nnumber = Nieuwnumber + 1
-print ("35 + 1 =")
+# variable + 
+Nnumber = 35 + 10
+print ("35 + 10 =")
 print ( Nnumber )
 print ("--------------------------------------------")
-Getal = Nnumber / 2
-print ("36 / 2 =")
+#optellen van + en =
+Ninumber = Nnumber + Number
+print ("45 + 45 =")
+print ( Nnumber )
+print ("--------------------------------------------")
+
+# nieuwe variabele =
+Number= 50
+print ("Number = ")
+print ( Number )
+print ("--------------------------------------------")
+# Variable /
+Getal = 30 / 2
+print ("300 / 2 =")
 print ( Getal)
 print ("--------------------------------------------")
+# delen van / en =
+Num = Getal / Number
+print ("150 / 50 =")
+print ( Num )
+print ("--------------------------------------------")
+
 restWaarde = Getal % 4
 print ("18 % 4 =")
-print ( restWaarde ) 
+print ( restWaarde )
+
 
