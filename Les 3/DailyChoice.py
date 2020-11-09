59f1088e4981c74238b01fd40c6f5f6c3833473e
Michelle <Minoord18@outlook.com>
2020-11-09 16:24:52 +0100

    Leerjaar 1 Periode 1 les 3

---
+++ Les 3/DailyChoice.py
@@ -0,0 +1,66 @@
+print(" Dit is een make your own choice story. Dat houd in dat je keuzes krijgt en dat je daar uit moet kiezen.")
+print(" Het woord dat in hoofdletters staat is een keuze. type je keuze in kleine letters in. Als je iets anders in type krijg je een andere keuze. Wat die keuze is, is een verassing. ")
+print("Ik hoop dat je het leuk vind! ")
+print(" Laten we nu beginnen!") 
+ 
+print( " ")
+print(" Je komt net uit je werk.  Wat ga je doen? Ga je even een rondje LOPEN of een DUTJE doen.  ")
+choice = input()
+if choice == 'lopen' : 
+     print(" je hebt gelukkig nog je jas en schoenen aan en je loopt zo de deur uit. Na 20 Minuten lopen heb je er al weer genoeg van , dus loop je weer terug. Als je thuis komt kom je er achter dat je de deur niet opslot  had gedaan. Je kijkt snel rond of er iets gestolen is, maar je komt er achter dat er niets mist. Je bent opgelucht, maar je print het goed in je hoofd dat de volgende keer je minder geluk zou hebben.  ")
+elif choice == 'dutje' : 
+     print(" Je trek je jas en je schoenen uit en gaat op de bank liggen en voordat je het weet lig je te slapen. Wanneer je wakker wordt hoor je je maag rommelen en besluit wat te gaan eten.")
+else: 
+     print( " Je besluit om verder te gaan werken aan je werk. Je hebt toch nog veel te doen, waarom niet nu? ")
+
+
+print(" ")
+print(" Het is rond etenstijd en je hebt trek in pizza, maar ga je  PIZZA HALEN of BESTEL je?") 
+choice = input()
+if choice == 'pizza halen' :
+     print(" Je gaat het huis uit om pizza te halen en komt onderweg een vriend tegen die ook eten gaat halen. Jullie doen samen boodschappen en alles gaat goed. Je voelt je goed als je thuis komt met de pizza en een zak   chips voor vanavond.")
+elif choice == 'bestel' : 
+     print(" Je bestelt een pizza en even later komt de bezorger met de pizza aan, waneer je de pizza zit te eten bedenk je dat je niks lekkers hebt voor vanavond en je kijkt op de klok of je nog even wat lekkers kan gaan halen, maar je ziet dat het er al te laat voor is. Was je toch maar een pizza gaan halen")
+else: 
+     print( " Je hebt besloten een pakje noodles open te maken en dat te eten. Noodles zijn lekker, maar je had liever de pizza")
+
+
+print( " ")
+print(" Nu je hebt gegeten, besluit je om tv te kijken. Wat ga je kijken? Een DRAMA serie over dokters of een HORROR film over een clown?")
+choice = input()
+if choice == 'drama' : 
+     print(" je kijkt de drama en besluit dat je nooit een dokter wilt worden. De serie zelf was oke, maar je vraagt af  of de horror film beter was.")
+elif choice == 'horror' : 
+     print(" Wanneer de film is af gelopen, zit je te trillen van de angst. Je had liever de drama serie gekeken, maar je was blij dat je deze film had gekeken")
+else: 
+     print( "Je kan niet kiezen tussen de twee dus ga je maar je favoriete serie bingewatchen. Je hebt deze serie zo vaak gekeken en toch voelt het alsof je het de eerste keer kijkt. Als de serie af is gelopen is,  heb je er geen spijt van gekregen dat je deze serie nog een keer hebt gekeken")
+
+
+print( " ")
+print(" Je zit tv te kijken en je hoort de deurbel gaan. Ga je de deur OPEN doen of blijf je TV kijken ")
+choice = input()
+if choice == 'open' : 
+     print(" je zet de tv op pauze en loopt naar de deur. Als je de deur open doet zie je een meisje voor je staan. Je kijkt beter en ziet dat het je buurmeisje is. Ze zegt dat zij en haar broertje voetbal aan het spelen   waren en dat hun bal in je tuin ligt. Je nodig haar uit om naar binnen te komen en samen naar de tuin te lopen om de bal te halen. Ze loopt met je mee naar de tuin en pakt haar bal. Ze bedankt je en gaat        vervolgens via de achtertuin weg. Je gaat weer naar binnen en gaat verder met je serie kijken. Je hoopt dat dit niet vaker gebeurd, want je bent nu vergeten wat er net op de tv is gebeurd.")
+elif choice == 'tv' : 
+     print(" Je blijft zitten met een lichtelijk schuld gevoel. Je hoort de belen vervolgens de hele avond niet. Blijkbaar was het niet belangrijk, anders hadden ze nog een keer aangebeld.")
+else: 
+     print( " Je besluit om toch op te staan om te kijken wie er aan de deur is. Als je de deur open doet zie je niemand staan, vervolgens hoor je achter je iets vallen. Je doet de deur dicht en je ziet dat een foto van jouw en je familie kapot op de grond is gevallen in de woonkamer. Je ruimte het op en gaat verder met tv kijken. Dit moet natuurlijk net jou overkomen. ")
+
+
+
+print(" ")
+print(" Nu je klaar bent met tv kijken sta je weer voor een keuze: Ga je kijken of je vrienden online zijn en nog een GAME met hun spelen of ga je nog een BOEK lezen?")
+choice = input() 
+if choice == 'game' : 
+     print("Je kijkt of je vrienden online zijn en komt er achter dat ze net waren begonnen met spelen, dus je wacht tot ze klaar zijn en begint mee te doen. Als je een paar uur later op de klok kijkt zie je al dat het laat is en neemt afscheid van je vrienden en gaat naar bed toe. Het duurde even voordat je in slaap viel, maar toen je in slaap viel had je wel het lekkerst geslapen dan je in een lange tijd had gedaan.")
+elif choice == 'boek' : 
+     print(" Je loopt naar de boekenkast en pakt daar een boek  waarvan de kaft jou het meest aanspreekt. je begint al snel in het verhaal te raken en voordat je het weet ben je al half verwegen het boek. Je ziet hoelaat  het is en gaat naar bed. Je bent nog paar keer wakker geworden door wat lawaai van buiten, maar viel wel telkens heel snel in slaap.")
+else: 
+     print( "Je besluit om naar bed te gaan, je was al aardig moe van het tv kijken plus je hebt morgen weer werk. Als je in je bed ligt, zit je nog wat te fantaseren en valt daarna in slaap")
+
+
+print(" ")
+print(" Je staat de volgende dag op een voelt alsof je gisteren avond niet jezelf was. Het voelde alsof je niet je eigen keuzes had gemaakt, maar je laat dat gevoel met rust en gaat verder met je dag.")
+
+print(" ")
+print(" Creator:Dit is het einde van het verhaal. Bedankt voor het testen! Hoop dat je het leuk vond!")
