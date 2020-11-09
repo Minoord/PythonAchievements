ad44fff98f500617d0e6090d0af5a8d5fa0a1905
Michelle <Minoord18@outlook.com>
2020-11-09 16:23:09 +0100

    Leerjaar 1 Periode 1 les 2

---
+++ Les 2/PYTB1L2MyFirstOperations/PYTB1L2MyFirstOperations.py
@@ -0,0 +1,80 @@
+L = 2 + 2
+print( "2 + 2 =")
+print (L)
+print ("--------------------------------------------")
+X = 3-2
+print ("3 - 2 =") 
+print(X)
+print ("--------------------------------------------")
+G = 3*92
+print ("3 * 92 =") 
+print(G)
+print ("--------------------------------------------")
+H = 40 / 5
+print ("40 / 5 =") 
+print ( H )
+print ("--------------------------------------------")
+
+# variable =
+Number= 45
+print ("Number= 45")
+print ( Number )
+print ("--------------------------------------------")
+# variable + 
+Nnumber = 35 + 10
+print ("35 + 10 =")
+print ( Nnumber )
+print ("--------------------------------------------")
+#optellen van + en =
+Ninumber = Nnumber + Number
+print ("45 + 45 =")
+print ( Ninumber )
+print ("--------------------------------------------")
+
+# nieuwe variabele =
+Number= 50
+print ("Number = ")
+print ( Number )
+print ("--------------------------------------------")
+# Variable /
+Getal = 30 / 2
+print ("300 / 2 =")
+print ( Getal)
+print ("--------------------------------------------")
+# delen van / en =
+Num = Getal / Number
+print ("150 / 50 =")
+print ( Num )
+print ("--------------------------------------------")
+
+# Gedeeld door
+Num /= 2
+print (" 0,3 / 2 =")
+print (Num)
+print ("--------------------------------------------")
+# Min 
+Num -= 1
+print (" 0,15 - 1 =")
+print (Num)
+print ("--------------------------------------------")
+# keer
+Num *= 8
+print (" -0,85 * 8 =")
+print (Num)
+print ("--------------------------------------------")
+#plus
+Num += 5
+print (" -6,8 + 5 =")
+print (Num)
+print ("--------------------------------------------")
+# restwaarde
+Num %= 8
+print (" -1,79 % 8  =")
+print (Num)
+print ("--------------------------------------------")
+
+restWaarde = Getal % 4
+print ("18 % 4 =")
+print ( restWaarde )
+
+
